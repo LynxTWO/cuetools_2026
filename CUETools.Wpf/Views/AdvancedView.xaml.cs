1734548e10e334352253c7d2ea06dc33138fa782
LynxTWO <usaf.the.boyd@gmail.com>
2026-07-25 16:35:21 -0400

    feat(settings): Advanced page - surface the 21 engine settings the UI dropped (incl. Create TOC files)

---
+++ CUETools.Wpf/Views/AdvancedView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace CUETools.Wpf.Views;
+
+public partial class AdvancedView : UserControl
+{
+    public AdvancedView()
+    {
+        InitializeComponent();
+    }
+}
